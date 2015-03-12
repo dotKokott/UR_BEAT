8bf8c029daa1a300734ae4b5a178632a97312d5c
Christian Kokott <christian.kokott@gmail.com>
2015-03-12 23:40:20 +0100

    initial test

---
+++ source/Assets/Beat.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beat : MonoBehaviour {
+
+	public Tone StartTone;
+
+	private Tone target;
+
+	public float Speed;
+
+	void Start () {
+		transform.position = StartTone.transform.position;
+		target = StartTone.Next;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dist = target.transform.position - transform.position;
+		if (dist.magnitude < 0.1f) {
+			transform.position = target.transform.position;
+			target.Play();
+
+			target = target.Next;
+		} else {
+			transform.position += dist.normalized * Speed * Time.deltaTime;
+		}
+	}
+}
---
+++ source/Assets/Tone.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tone : MonoBehaviour {
+
+	public Tone Next;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play() {
+		source.PlayOneShot(source.clip);
+		//iTween.ScaleBy(this.gameObject, iTween.Hash(
+		//	"amount", new Vector3(1.1f, 1.1f, 1.1f), 
+		//	"time", 0.1f, 
+		//	"looptype", iTween.LoopType.pingPong,
+		//	"oncomplete", "OnComplete"
+		//	));		
+	}
+
+	void OnComplete() {
+		iTween.Stop(this.gameObject);
+	}
+}
